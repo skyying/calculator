4cdaa9eec27fa58b7e9d94d5dfb3ee602a286fb5
Chien Wen Liu <dreamuk@gmail.com>
2018-11-04 14:23:59 +0800

    update webpack config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,84 @@
-const path = require("path");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-module.exports = {
-    entry:{
-        app : "./src/app.js",
-    },
-    devtool: "source-map", 
-    devServer : {
-        contentBase : "./dist"
-    },
-    output : {
-        filename : "[name].bundle.js",
-        path: path.resolve(__dirname, "dist")
-    },
-    plugins : [
-        new CleanWebpackPlugin(["dist"]),
-        new HtmlWebpackPlugin({
-            template : "./src/index.html"
-        }),
-    ],
-    module : {
-        rules: [
-            { 
-                test : /\.s?css/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader"
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,  
-                use : [
-                    "file-loader"
-                ]
-            },
-            {
-                test: /\.js$/,
-                loader: "babel-loader",
-                exclude: /node_modules/,
-                query: {
-                    presets: ["es2015"]
-                }
-            }
-        ]
-    }
+
+const path = require("path");
+const glob = require("glob");
+const merge = require("webpack-merge");
+const parts = require("./config/webpack.parts.js");
+
+const PATHS = {
+  app: path.join(__dirname, "src"),
+  build: path.resolve(__dirname, "dist")
 };
 
+const commonConfig = merge([
+  {
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "webpack demo",
+        template: "./src/index.html"
+      })
+    ]
+  },
+  parts.loadJavaScript({include: PATHS.app})
+]);
+
+const productionConfig = merge([
+  {
+    output: {
+      chunkFilename: "[name].[chunkhash:4].js",
+      filename: "[name].[chunkhash:4].js"
+    },
+    recordsPath: path.resolve(__dirname, "records.json")
+  },
+  parts.clean("dist"),
+  parts.minifyJavaScript(),
+  parts.minifyCSS({
+    options: {
+      discardComments: {
+        removeAll: true
+      },
+      safe: true
+    }
+  }),
+  parts.generateSourceMaps({type: "source-map"}),
+  parts.extractCSS({}),
+  parts.loadImages({
+    options: {
+      limit: 10000,
+      name: "[name].[hash:4].[ext]"
+    }
+  }),
+  parts.loadSVG({}),
+  parts.loadFonts(),
+  {
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: "vendor",
+            chunks: "initial"
+          }
+        }
+      },
+      runtimeChunk: {
+        name: "manifest"
+      }
+    }
+  }
+]);
+
+const developmentConfig = merge([
+  parts.devServer({}),
+  parts.loadCSS(),
+  parts.loadImages(),
+  parts.loadSVG({}),
+  parts.loadFonts()
+]);
+
+module.exports = mode => {
+  if (mode === "production") {
+    return merge(commonConfig, productionConfig, {mode});
+  }
+  return merge(commonConfig, developmentConfig, {mode});
+};
